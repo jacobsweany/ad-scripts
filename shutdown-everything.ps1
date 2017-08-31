63ff97be4c1e95c349a392bb5a505b7e8e656eef
jacobsweany <30737348+jacobsweany@users.noreply.github.com>
2017-08-30 19:09:43 -0700

    Create shutdown-everything.ps1

---
+++ shutdown-everything.ps1
@@ -0,0 +1,41 @@
+# Test connection, shut down if online. Either way log to file
+foreach ($Computer in $Computers) {
+    $test = Test-Connection -ComputerName $Computer -Count 1 -Quiet -BufferSize 16
+     if ($test) {
+        Write-Output "$Computer" | Out-File $Online -Append
+        Stop-Computer $Computer -Force
+     }
+     else {
+        Write-Output "$Computer" | Out-File $Offline -Append
+     }
+}
+
+Start-Sleep -Seconds 120
+
+# Test connection, shut down if online. Either way log to file
+foreach ($Computer in $Computers) {
+    $test = Test-Connection -ComputerName $Computer -Count 1 -Quiet -BufferSize 16
+     if ($test) {
+        Write-Output "$Computer" | Out-File $Online -Append
+        Stop-Computer $Computer -Force
+     }
+     else {
+        Write-Output "$Computer" | Out-File $Offline -Append
+     }
+}
+
+Start-Sleep -Seconds 120
+
+foreach ($Computer in $Computers) {
+    Stop-Computer -ComputerName $Computer -Force
+}
+
+Stop-Computer -ComputerName SERVERNAME1 -Force
+Write-Output "$Computer" | Out-File $Offline -Append
+
+while ($test = Test-Connection -ComputerName SERVERNAME1 -Count 1 -Quiet) {
+    Write-Output "SERVERNAME1 is online still"
+    Start-Sleep -Seconds 10
+}
+
+Stop-Computer -Force
