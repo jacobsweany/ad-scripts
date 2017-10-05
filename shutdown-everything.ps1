1965726beb33f398dee6102fee174b4b1ece641a
jacobsweany <30737348+jacobsweany@users.noreply.github.com>
2017-10-05 15:20:05 -0700

    Update shutdown-everything.ps1

--- shutdown-everything.ps1
+++ shutdown-everything.ps1
@@ -6,6 +6,14 @@ $WorkstationsClean = "\\path\file-clean.txt"
 $Online = "\\path\Online.txt"
 $Offline = "\\path\Offline.txt"
 
+$OnlineContent = Get-Content $Online
+Get-Content $Workstations | Sort-Object | Get-Unique | Set-Content $WorkstationsClean
+
+Clear-Content $Online
+Clear-Content $Offline
+
+
+
 foreach ($Computer in $Computers) {
     $test = Test-Connection -ComputerName $Computer -Count 1 -Quiet -BufferSize 16
      if ($test) {
@@ -17,9 +25,7 @@ foreach ($Computer in $Computers) {
      }
 }
 
-Start-Sleep -Seconds 120
-
-# Test connection, shut down if online. Either way log to file
+# Test connection, shut down if online. Either way log to file. First pass.
 foreach ($Computer in $Computers) {
     $test = Test-Connection -ComputerName $Computer -Count 1 -Quiet -BufferSize 16
      if ($test) {
@@ -33,7 +39,26 @@ foreach ($Computer in $Computers) {
      }
 }
 
-Start-Sleep -Seconds 120
+Start-Sleep -Seconds 10
+# Loop entire process until all online computers are no longer pingable
+while $OnlineContent -ne $null) {
+    # Refresh input file
+    $OnlineContent = Get-Content $Online
+    # Test connection, shut down if online. Either way log to file. First pass.
+    foreach ($Computer in $Computers) {
+        $test = Test-Connection -ComputerName $Computer -Count 1 -Quiet -BufferSize 16
+         if ($test) {
+            Write-Output "$Computer" | Out-File $Online -Append
+            Stop-Computer $Computer -Force
+         }
+         else {
+            Write-Output "$Computer" | Out-File $Offline -Append
+            # Remove computer name out of Online
+            (Get-Content $Online) -notmatch "$Computer" | Out-File $Online
+         }
+    }
+}
+
 
 foreach ($Computer in $Computers) {
     Stop-Computer -ComputerName $Computer -Force
@@ -47,4 +72,5 @@ while ($test = Test-Connection -ComputerName SERVERNAME1 -Count 1 -Quiet) {
     Start-Sleep -Seconds 10
 }
 
-Stop-Computer -Force
+# Shut down local system
+# Stop-Computer -Force
