5bf505e53a3733a1e403b572a432909043841abd
jacobsweany <30737348+jacobsweany@users.noreply.github.com>
2018-07-02 17:33:13 -0700

    Update SyncScopes-DHCP.ps1

--- SyncScopes-DHCP.ps1
+++ SyncScopes-DHCP.ps1
@@ -1,3 +1,8 @@
+# Create DHCP PSSession
+$DHCPSession = New-PSSession -ComputerName "DHCPSERVERNAME"
+Invoke-Command -Session $DHCPSession { Import-Module DHCPServer }
+Import-PSSession -Session $DHCPSession -Module DHCPServer -AllowClobber
+
 $DHCPServers = Get-DHCPServerInDC | Select -ExpandProperty DnsName
 $Query = Get-DhcpServerv4Scope | Where-Object { $_.Name -notlike "*VoIP*" -and ( $_.Name -notlike "*Printer*") -and ($_.State -eq "Active") } | select Name, StartRange, EndRange, Description
 $RunBank = New-Object psobject @{}
